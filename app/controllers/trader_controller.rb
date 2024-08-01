a8f62bbd102b7d12eff8fa5f2681b7a940765fd4
Ivan Nollora <palakol80@gmail.com>
2024-08-02 03:34:29 +1000

    Added security feature and create method

--- app/controllers/trader_controller.rb
+++ app/controllers/trader_controller.rb
@@ -2,8 +2,18 @@ class TraderController < ApplicationController
   before_action :authenticate_user!
   # before_action :admin_only
   # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user, only: %i[index]
 
   def index
-    @hello = "greetings!"
   end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: current_user.id)
+    unless @user
+      redirect_to root_path, alert: 'Trader not found'
+    end
+  end
+
 end
